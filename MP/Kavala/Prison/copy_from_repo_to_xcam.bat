399ae1c05fe8504a9d52089717bde4a0d654c7e4
Dr. Ignaz M <9mmfullclip@gmail.com>
2016-03-25 23:56:30 +0100

    mission

---
+++ MP/Kavala/Prison/copy_from_repo_to_xcam.bat
@@ -0,0 +1,15 @@
+@echo off
+
+@echo.
+@echo THIS WILL REMOVE YOUR LOCAL FILES.
+@echo IT WILL DESTROY THE WORK YOU DID LOCALLY.
+@echo.
+@echo PRESS space TO CONTINUE. YOUR LOCAL WORK WILL BE DESTROYED.
+@echo.
+@echo TO ABORT JUST CLOSE THIS WINDOW AND DO NOT PRESS space.
+@echo.
+
+@pause
+
+@copy *.ini "D:\Steam\steamapps\common\Arma 3\@inidbi\db\xCam\Library_Pro\"
+
---
+++ MP/Kavala/Prison/copy_from_xcam_to_repo.bat
@@ -0,0 +1,9 @@
+@echo off
+
+copy "D:\Steam\steamapps\common\Arma 3\@inidbi\db\xCam\Library_Pro\_xCamProjects.ini" .
+
+copy "D:\Steam\steamapps\common\Arma 3\@inidbi\db\xCam\Library_Pro\Altis_KavalaPrison.ini" .
+
+copy "D:\Steam\steamapps\common\Arma 3\mission_Altis_KavalaPrison.sqm" .
+
+del "D:\Steam\steamapps\common\Arma 3"\mission_*.sqm
